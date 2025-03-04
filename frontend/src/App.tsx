987d572eeec9aa2416f81cee4a23a105fcfe165c
Florian Barrali <barrali.florian@gmail.com>
2025-03-04 13:49:16 +0100

    feat(front): configuration du routeur et intégration dans l'app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 ﻿import React from 'react';
+import Router from './Router';
 import { ThemeProvider } from '@/contexts/ThemeContext';
-import ThemeToggle from '@/components/common/ThemeToggle';
 import './styles/theme.css';
 import './styles/theme-components.css';
 import './index.css';
@@ -8,16 +8,7 @@ import './index.css';
 function App() {
   return (
     <ThemeProvider>
-      <div className="app">
-        <header className="app-header">
-          <h1>PixelArt App</h1>
-          <ThemeToggle className="theme-toggle-button" />
-        </header>
-        <main className="app-main">
-          <p>Bienvenue sur l'application PixelArt!</p>
-          <p>Cette application vous permet de créer et partager des PixelBoards avec la communauté.</p>
-        </main>
-      </div>
+      <Router />
     </ThemeProvider>
   );
 }
