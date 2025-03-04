00cb9dfc225eed134c8dbbaa250d911d8f641d19
Florian Barrali <barrali.florian@gmail.com>
2025-03-04 15:32:21 +0100

    fix(frontend): corriger l'affichage de certains éléments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Router from './Router';
 import { ThemeProvider } from '@/contexts/ThemeContext';
 import './styles/theme.css';
 import './styles/theme-components.css';
+import './styles/auth-forms.css';
 import './index.css';
 
 function App() {
